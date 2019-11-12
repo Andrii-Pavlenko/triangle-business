d86894120ba79a22739697c2fc092e1d766eb6c9
Andrii <farikpite@gmail.com>
2019-11-13 01:35:02 +0200

    final version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Icon } from 'semantic-ui-react'
 import './App.css';
+
+const text = <p className="text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+   Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus 
+   et magnis dis parturient montes, nascetur</p>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container__left">
+        <div className="container__left__upper">
+          <div className="container__left__upper__header">
+            <span className="first">LOREM</span>
+            <span className="container__left__upper__header__second">IPSUM</span>
+          </div>
+          {text}
+          <div className="container__left__upper__romb_rotator">
+            <div className="container__left__upper__romb"></div>
+            <div className="icon container__left__upper__icon__circle">
+              <Icon name='dollar sign' className="container__left__upper__icon" />
+            </div>    
+          </div>
+                
+        </div>
+        <div className="container__left__lower">
+          <div className="container__left__lower__header">
+            <span className="first">LOREM</span>
+            <span className="container__left__lower__header__second">IPSUM</span>
+          </div>
+          {text}
+          <div className="container__left__lower__romb_rotator">
+            <div className="container__left__lower__romb"></div>
+            <div className="container__left__lower__icon">
+              <div className="container__left__lower__icon__first"></div>
+              <div className="container__left__lower__icon__second"></div>
+              <div className="container__left__lower__icon__third"></div>
+            </div>
+          </div>          
+        </div>        
+      </div>
+      <div className="container__right">
+        <div className="container__right__upper">
+          <div className="container__right__upper__header">
+            <span className="first">LOREM</span>
+            <span className="container__right__upper__header__second">IPSUM</span>
+          </div>
+          {text}
+          <div className="container__right__upper__romb_rotator">
+            <div className="container__right__upper__romb"></div>  
+            <Icon name='search' className="icon container__right__upper__icon" />
+          </div>          
+        </div>       
+        <div className="container__right__lower">
+          <div className="container__right__lower__header">
+            <span className="first">LOREM</span>
+            <span className="container__right__lower__header__second">IPSUM</span>
+          </div>
+          {text}
+          <div className="container__right__lower__romb_rotator">
+            <div className="container__right__lower__romb"></div>
+            <Icon name='user' className="icon container__right__lower__icon" />
+          </div>          
+        </div>        
+      </div>
     </div>
   );
 }
