5bc5bd9af322c7c65f7323786de15edd87d37c6b
Andrii <farikpite@gmail.com>
2019-11-13 01:31:52 +0200

    Updates

---
+++ precache-manifest.818847004b9f26dc4f0cc721fc92f3bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e9cbb428c3256a78d8a3fc2e649dab",
+    "url": "/triangle-business/index.html"
+  },
+  {
+    "revision": "bf6a9dcca09e4f32c522",
+    "url": "/triangle-business/static/css/main.0222b7ab.chunk.css"
+  },
+  {
+    "revision": "56e81d44b9e8d4414a73",
+    "url": "/triangle-business/static/js/2.cf71fc8f.chunk.js"
+  },
+  {
+    "revision": "bf6a9dcca09e4f32c522",
+    "url": "/triangle-business/static/js/main.242f8e86.chunk.js"
+  },
+  {
+    "revision": "2c8f5ed7fe170ec19eff",
+    "url": "/triangle-business/static/js/runtime-main.571959f2.js"
+  }
+]);
